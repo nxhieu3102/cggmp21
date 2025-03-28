7421584b73f310abe4bead68edff95532a9bc299
NTTVy03 <trucvy05062003@gmail.com>
2025-03-28 11:58:53 +0700

    p2p_example: create mpc party

--- examples/p2p_example/src/main.rs
+++ examples/p2p_example/src/main.rs
@@ -1,15 +1,20 @@
 mod config;
 mod handlers;
-mod message;
 mod node;
 
 use anyhow::Result;
 use config::load_config;
 use futures::StreamExt;
 use node::Node;
+use rand::rngs::OsRng;
+
+type E = generic_ec::curves::Secp256k1;
+type D = sha2::Sha256;
+type L = cggmp21::security_level::SecurityLevel128;
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    // get config
     let args: Vec<String> = std::env::args().collect();
     if args.len() < 2 {
         eprintln!("Usage: cargo run --bin p2p_example <config file>");
@@ -17,7 +22,10 @@ async fn main() -> Result<()> {
     }
 
     let config = load_config(&args[1])?;
-    let (node, mut incoming, mut outgoing) = Node::new(config.node.address).await?;
+
+    // set up network
+    let (node, mut incoming, mut outgoing) =
+        Node::<cggmp21::keygen::msg::threshold::Msg<E, L, D>>::new(config.node.address).await?;
 
     for peer in config.peers {
         if peer.id < config.node.id {
@@ -29,11 +37,29 @@ async fn main() -> Result<()> {
         }
     }
 
-    tokio::spawn(async move {
-        while let Some(Ok(incoming)) = incoming.next().await {
-            println!("Received: {:?}", incoming.msg);
-        }
-    });
+    // sleep for 10 seconds to allow all nodes to start
+    tokio::time::sleep(std::time::Duration::from_secs(10)).await;
+
+    // set up MPC
+    let delivery = (
+        incoming.map(|msg| msg.map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e))),
+        outgoing,
+    );
+    let party = round_based::MpcParty::connected(delivery);
+
+    // DKG
+    let eid = cggmp21::ExecutionId::new(b"execution id, unique per protocol execution");
+    let i = config.node.id - 1;
+    let n = 3;
+    let t = 2;
+
+    println!("Starting DKG with id: {}, n: {}, t: {}", i, n, t);
+
+    let _incomplete_key_share =
+        cggmp21::keygen::<E>(eid, i.try_into().expect("Can not parse id"), n)
+            .set_threshold(t)
+            .start(&mut OsRng, party)
+            .await?;
 
     tokio::signal::ctrl_c().await?;
     Ok(())
--- examples/p2p_example/src/node.rs
+++ examples/p2p_example/src/node.rs
@@ -1,7 +1,7 @@
-use crate::message::{Incoming, Msg, Outgoing};
 use anyhow::Result;
 use futures::{Sink, SinkExt, Stream, StreamExt};
 use futures_channel::mpsc;
+use round_based::{Incoming, Outgoing};
 use std::collections::HashMap;
 use std::net::SocketAddr;
 use std::sync::Arc;
@@ -11,18 +11,21 @@ use tokio::{
     sync::{RwLock, mpsc as tokio_mpsc},
 };
 
-pub struct Node {
+pub struct Node<M> {
     address: SocketAddr,
-    peers: Arc<RwLock<HashMap<SocketAddr, tokio_mpsc::Sender<Msg>>>>,
+    peers: Arc<RwLock<HashMap<SocketAddr, tokio_mpsc::Sender<M>>>>,
 }
 
-impl Node {
+impl<M> Node<M>
+where
+    M: serde::Serialize + Send + 'static,
+{
     pub async fn new(
         address: SocketAddr,
     ) -> Result<(
         Self,
-        impl Stream<Item = Result<Incoming<Msg>>>,
-        impl Sink<Outgoing<Msg>, Error = mpsc::SendError>,
+        impl Stream<Item = Result<Incoming<M>>>,
+        impl Sink<Outgoing<M>, Error = mpsc::SendError>,
     )> {
         let peers = Arc::new(RwLock::new(HashMap::new()));
         let (incoming_tx, incoming_rx) = mpsc::channel(32);
@@ -38,21 +41,23 @@ impl Node {
 
         tokio::spawn(async move {
             while let Ok((stream, address)) = listener.accept().await {
-                super::handlers::handle_connection(
-                    stream,
-                    address,
-                    incoming_tx.clone(),
-                    peers_clone.clone(),
-                )
-                .await;
+                println!("Incoming connection from: {}", address);
+                // super::handlers::handle_connection(
+                //     stream,
+                //     address,
+                //     incoming_tx.clone(),
+                //     peers_clone.clone(),
+                // )
+                // .await;
             }
         });
 
         tokio::spawn(async move {
-            while let Some(msg) = outgoing_rx.next().await {
-                if let Err(e) = super::handlers::handle_outgoing(msg, &peers).await {
-                    eprintln!("Error sending message: {}", e);
-                }
+            while let Some(_msg) = outgoing_rx.next().await {
+                println!("Sending some messages");
+                // if let Err(e) = super::handlers::handle_outgoing(msg, &peers).await {
+                //     eprintln!("Error sending message: {}", e);
+                // }
             }
         });
 
@@ -68,7 +73,8 @@ impl Node {
 
         tokio::spawn(async move {
             while let Some(msg) = rx.recv().await {
-                if writer.write_all(msg.content.as_bytes()).await.is_err() {
+                let encoded_msg = bincode::serialize(&msg).expect("Failed to serialize message");
+                if writer.write_all(&encoded_msg).await.is_err() {
                     break;
                 }
                 if writer.flush().await.is_err() {
