6c0dee837074fb4fa85a1a6ce646152421bedcf9
NTTVy03 <trucvy05062003@gmail.com>
2025-03-28 09:51:05 +0700

    p2p_example: connect p2p via yaml config

--- examples/p2p_example/src/main.rs
+++ examples/p2p_example/src/main.rs
@@ -1,13 +1,17 @@
-use futures::{SinkExt, StreamExt, Stream, Sink};
+// cargo run --bin p2p_example test-data/p2p_example/dkg/node1.yaml
+// cargo run --bin p2p_example test-data/p2p_example/dkg/node2.yaml
+// cargo run --bin p2p_example test-data/p2p_example/dkg/node3.yaml
+// -------------
+use anyhow::Result;
+use futures::{Sink, SinkExt, Stream, StreamExt};
 use futures_channel::mpsc;
+use std::sync::Arc;
 use std::{collections::HashMap, net::SocketAddr};
 use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
     net::{TcpListener, TcpStream},
     sync::{RwLock, mpsc as tokio_mpsc},
-    io::{AsyncReadExt, AsyncWriteExt},
 };
-use anyhow::Result;
-use std::sync::Arc;
 
 // Message types
 #[derive(Debug, Clone)]
@@ -30,29 +34,35 @@ pub struct Outgoing<T> {
 
 // P2P Node structure
 pub struct Node {
-    addr: SocketAddr,
+    address: SocketAddr,
     peers: Arc<RwLock<HashMap<SocketAddr, tokio_mpsc::Sender<Msg>>>>,
 }
 
 impl Node {
-    pub async fn new(addr: SocketAddr) -> Result<(Self, impl Stream<Item = Result<Incoming<Msg>>>, impl Sink<Outgoing<Msg>, Error = mpsc::SendError>)> {
+    pub async fn new(
+        address: SocketAddr,
+    ) -> Result<(
+        Self,
+        impl Stream<Item = Result<Incoming<Msg>>>,
+        impl Sink<Outgoing<Msg>, Error = mpsc::SendError>,
+    )> {
         let peers = Arc::new(RwLock::new(HashMap::new()));
         let (incoming_tx, incoming_rx) = mpsc::channel(32);
         let (outgoing_tx, mut outgoing_rx) = mpsc::channel(32);
 
         let node = Node {
-            addr,
+            address,
             peers: peers.clone(),
         };
 
         // Start TCP listener
-        let listener = TcpListener::bind(addr).await?;
+        let listener = TcpListener::bind(address).await?;
         let peers_clone = peers.clone();
 
         // Handle incoming connections
         tokio::spawn(async move {
-            while let Ok((stream, addr)) = listener.accept().await {
-                handle_connection(stream, addr, incoming_tx.clone(), peers_clone.clone()).await;
+            while let Ok((stream, address)) = listener.accept().await {
+                handle_connection(stream, address, incoming_tx.clone(), peers_clone.clone()).await;
             }
         });
 
@@ -68,14 +78,14 @@ impl Node {
         Ok((node, incoming_rx.map(Ok), outgoing_tx))
     }
 
-    pub async fn connect(&self, addr: SocketAddr) -> Result<()> {
-        let stream = TcpStream::connect(addr).await?;
+    pub async fn connect(&self, address: SocketAddr) -> Result<()> {
+        let stream = TcpStream::connect(address).await?;
         let (tx, mut rx) = tokio_mpsc::channel(32);
-        self.peers.write().await.insert(addr, tx);
-        
+        self.peers.write().await.insert(address, tx);
+
         // Set up message forwarding from channel to TCP
         let (mut reader, mut writer) = tokio::io::split(stream);
-        
+
         // Forward messages from rx channel to the TCP connection
         tokio::spawn(async move {
             while let Some(msg) = rx.recv().await {
@@ -90,7 +100,7 @@ impl Node {
                 }
             }
         });
-        
+
         // A real implementation would also set up a reader for bidirectional communication
         Ok(())
     }
@@ -98,27 +108,27 @@ impl Node {
 
 async fn handle_connection(
     stream: TcpStream,
-    addr: SocketAddr,
+    address: SocketAddr,
     incoming_tx: mpsc::Sender<Incoming<Msg>>,
     peers: Arc<RwLock<HashMap<SocketAddr, tokio_mpsc::Sender<Msg>>>>,
 ) {
     // Split the TCP stream
     let (mut reader, mut writer) = tokio::io::split(stream);
-    
+
     // Create a channel for this peer
     let (tx, _rx) = tokio_mpsc::channel::<Msg>(32);
-    peers.write().await.insert(addr, tx);
-    
+    peers.write().await.insert(address, tx);
+
     // Read from TCP and forward to internal messaging
     let mut buffer = [0u8; 1024];
     let mut incoming_tx = incoming_tx;
-    
+
     tokio::spawn(async move {
         loop {
             match reader.read(&mut buffer).await {
                 Ok(0) => {
                     // Connection closed
-                    println!("Connection closed by peer: {}", addr);
+                    println!("Connection closed by peer: {}", address);
                     break;
                 }
                 Ok(n) => {
@@ -126,9 +136,9 @@ async fn handle_connection(
                     if let Ok(content) = String::from_utf8(buffer[..n].to_vec()) {
                         let msg = Msg {
                             content,
-                            from: addr,
+                            from: address,
                         };
-                        let incoming = Incoming { msg, from: addr };
+                        let incoming = Incoming { msg, from: address };
                         if let Err(e) = incoming_tx.send(incoming).await {
                             eprintln!("Failed to forward message internally: {}", e);
                             break;
@@ -142,7 +152,7 @@ async fn handle_connection(
             }
         }
         // Remove peer when connection ends
-        peers.write().await.remove(&addr);
+        peers.write().await.remove(&address);
     });
 }
 
@@ -163,36 +173,57 @@ async fn main() -> Result<()> {
     // Check command line args to determine which node to run
     let args: Vec<String> = std::env::args().collect();
     if args.len() < 2 {
-        eprintln!("Usage: cargo run --example p2p_example [1|2]");
-        eprintln!("  1: Run as node 1 (listen on 127.0.0.1:8081)");
-        eprintln!("  2: Run as node 2 (listen on 127.0.0.1:8082)");
+        eprintln!("Usage: cargo run --bin p2p_example <config file>");
+        eprintln!("Example: cargo run --bin p2p_example test-data/p2p_example/dkg/node1.yaml");
         return Ok(());
     }
 
-    match args[1].as_str() {
-        "1" => run_node1().await,
-        "2" => run_node2().await,
-        _ => {
-            eprintln!("Invalid argument. Use '1' or '2'.");
-            Ok(())
-        }
-    }
+    let config_file = args[1].as_str();
+    let config = load_config(config_file)?;
+
+    run_node(config).await
 }
 
-async fn run_node1() -> Result<()> {
-    let addr1 = "127.0.0.1:8081".parse()?;
-    let addr2 = "127.0.0.1:8082".parse()?;
-    
-    println!("Starting node 1 on {}", addr1);
-    let (node1, mut incoming1, mut outgoing1) = Node::new(addr1).await?;
-    
+// config file format
+#[derive(serde::Deserialize)]
+struct Config {
+    node: NodeConfig,
+    peers: Vec<PeerConfig>,
+}
+
+#[derive(serde::Deserialize)]
+struct NodeConfig {
+    id: usize,
+    address: SocketAddr,
+}
+
+#[derive(serde::Deserialize)]
+struct PeerConfig {
+    id: usize,
+    address: SocketAddr,
+}
+
+fn load_config(config_file: &str) -> Result<Config> {
+    let config = std::fs::read_to_string(config_file)?;
+    let config: Config = serde_yaml::from_str(&config)?;
+    Ok(config)
+}
+
+// Run a single node
+async fn run_node(config: Config) -> Result<()> {
+    println!(
+        "Starting node {} on {}",
+        config.node.id, config.node.address
+    );
+    let (node, mut incoming, mut outgoing) = Node::new(config.node.address).await?;
+
     // INTEGRATION WITH ROUND_BASED PROTOCOL
     // To use this p2p setup with the CGGMP21 protocols, you would need to:
     // 1. Create adapters from our Incoming/Outgoing to round_based::Incoming/Outgoing
     // 2. Connect them with MpcParty:
     //
     // let party = round_based::MpcParty::connected((
-    //    adapter_for_incoming_stream, 
+    //    adapter_for_incoming_stream,
     //    adapter_for_outgoing_sink
     // ));`
     //
@@ -200,58 +231,53 @@ async fn run_node1() -> Result<()> {
     // e.g.: cggmp21::signing(eid, i, &parties_indexes_at_keygen, &key_share)
     //         .sign(&mut OsRng, party, data_to_sign)
     //         .await?;
-    
-    // Give node2 time to start before trying to connect
-    println!("Waiting 2 seconds before connecting to node 2...");
-    tokio::time::sleep(tokio::time::Duration::from_secs(2)).await;
-    
-    println!("Connecting to node 2 at {}", addr2);
-    if let Err(e) = node1.connect(addr2).await {
-        eprintln!("Failed to connect to node 2: {}", e);
-    } else {
-        println!("Connected to node 2!");
-        
-        // Send message to node 2
-        let msg = Outgoing {
-            msg: Msg {
-                content: "Hello from node1!".to_string(),
-                from: addr1,
-            },
-            to: addr2,
-        };
-        println!("Sending message to node 2: {}", msg.msg.content);
-        if let Err(e) = outgoing1.send(msg).await {
-            eprintln!("Failed to send message: {}", e);
+
+    // Give other nodes time to start before trying to connect
+    println!("Waiting 5 seconds before connecting to other nodes...");
+    tokio::time::sleep(tokio::time::Duration::from_secs(5)).await;
+
+    for peer in config.peers {
+        // only connect to peers with lower address
+        // advoid 2 nodes connecting to each other
+        if peer.id >= config.node.id {
+            continue;
+        }
+
+        println!("Connecting to peer {} at {}", peer.id, peer.address);
+
+        if let Err(e) = node.connect(peer.address).await {
+            eprintln!("Failed to connect to peer: {}", e);
         } else {
-            println!("Message sent successfully!");
+            println!("Connected to peer!");
+
+            // Send message to that node
+            let msg = Outgoing {
+                msg: Msg {
+                    content: format!("Hello from node {}!", config.node.id),
+                    from: config.node.address,
+                },
+                to: peer.address,
+            };
+
+            println!("Sending message to node {}: {}", peer.id, msg.msg.content);
+            if let Err(e) = outgoing.send(msg).await {
+                eprintln!("Failed to send message: {}", e);
+            } else {
+                println!("Message sent successfully!");
+            }
         }
     }
 
     // Also listen for incoming messages
     tokio::spawn(async move {
         println!("Listening for incoming messages...");
-        while let Some(Ok(incoming)) = incoming1.next().await {
-            println!("Node1 received: {:?}", incoming.msg);
+        while let Some(Ok(incoming)) = incoming.next().await {
+            println!("Node {} received: {:?}", config.node.id, incoming.msg);
         }
     });
-    
+
     // Keep the main thread running
     println!("Press Ctrl+C to exit");
     tokio::signal::ctrl_c().await?;
     Ok(())
 }
-
-async fn run_node2() -> Result<()> {
-    let addr2 = "127.0.0.1:8082".parse()?;
-    
-    println!("Starting node 2 on {}", addr2);
-    let (node2, mut incoming2, _outgoing2) = Node::new(addr2).await?;
-    
-    // Listen for incoming messages
-    println!("Listening for incoming messages...");
-    while let Some(Ok(incoming)) = incoming2.next().await {
-        println!("Node2 received: {:?} from {}", incoming.msg.content, incoming.from);
-    }
-    
-    Ok(())
-} 
